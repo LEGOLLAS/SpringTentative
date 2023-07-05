ea25d997c2431b88981d4005c73940e7f6c62626
DESKTOP-O403M3T\user <ta>
2023-07-05 20:11:56 +0900

    feat : Spring tentative project upload - 20230705 from marondal

---
+++ src/test/java/com/gurwl/test/SpringTentativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurwl.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTentativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
